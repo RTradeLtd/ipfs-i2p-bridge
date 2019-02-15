a7d504be355ba715a3656d682ed853aeeb971351
idk <problemsolver@openmailbox.org>
2019-02-15 01:00:52 -0500

    change countdown again

--- commands/start.sh
+++ commands/start.sh
@@ -2,8 +2,10 @@
 
 countdown() {
     l="30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0"
+    echo "Giving the network time to start up."
     for i in $l; do
-        echo $i
+        echo "$i... "
+        sleep 1
     done
 }
 
