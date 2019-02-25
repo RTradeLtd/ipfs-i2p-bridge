30b3dfd8866d9336b7cb4018914bff779bc29ddc
idk <problemsolver@openmailbox.org>
2019-02-25 10:12:55 -0500

    updatE

--- Dockerfiles/i2p.Dockerfile
+++ Dockerfiles/i2p.Dockerfile
@@ -1,1 +1,9 @@
 FROM geti2p/i2p
+USER root
+COPY clients.config /usr/share/i2p/i2p-config/clients.config
+RUN cat /usr/share/i2p/i2p-config/clients.config > /var/lib/i2p/clients.config
+CMD cat /usr/share/i2p/i2p-config/clients.config
+VOLUME /var/lib/i2p
+USER i2psvc
+ENTRYPOINT ["/usr/bin/i2prouter"]
+CMD ["console"]
--- commands/start.sh
+++ commands/start.sh
@@ -6,7 +6,7 @@ countdown() {
     l="30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0"
     echo "Giving the network time to start up."
     for i in $l; do
-        echo "$i... "
+        echo -n "$i... "
         sleep 1
     done
 }
@@ -14,20 +14,28 @@ countdown() {
 starti2p(){
     createnetwork
     docker run --detach \
-        --ip 172.92.92.1 \
+        --ip 172.92.92.2 \
+        --name "i2pbridge" \
         --hostname "i2pbridge" \
         --network $NETWORK_NAME \
         --volume i2p:/var/lib/i2p:Z \
-        $(whoami)/i2p.ipfs
+        $(whoami)/i2p.ipfs; true
+    docker start i2pbridge
+    echo "Starting i2p"
     countdown
 }
 
 startipfs(){
+    if [ -z "$BRIGE_NODE_ID" ]; then
+        BRIDGE_NODE_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n 1)
+    fi
     createnetwork
-    docker run \
-        --hostname "ipfsbridge"$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n 1) \
+    docker run --detach \
+        --name "ipfsbridge-$BRIDGE_NODE_ID" \
+        --hostname "ipfsbridge-$BRIDGE_NODE_ID" \
         --network $NETWORK_NAME \
         $(whoami)/ipfs.i2p
+    docker start "ipfsbridge-$BRIDGE_NODE_ID"
     countdown
 }
 
